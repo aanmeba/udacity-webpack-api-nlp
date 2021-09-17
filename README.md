5fc53f066928465af56e58d04c5bf917e737b779
aanmeba <85666232+aanmeba@users.noreply.github.com>
2021-09-17 22:27:00 +1000

    update contents

---
+++ README.md
@@ -0,0 +1,20 @@
+# Evaluate a News Article with Natural Language project
+
+## Overview
+
+The goal of this project is to give the student practice with:
+
+- Setting up Webpack
+- Sass styles
+- Webpack Loaders and Plugins
+- Creating layouts and page design
+- Service workers
+- Using APIs and creating requests to external urls
+
+In order to develop this project, we are using the concept of Natural language processing and APIs created with the purpose of evaluating Natural language.
+
+## Project-Rubric
+
+The project is created in accordance to the following specifications:
+
+- <a href="https://review.udacity.com/#!/rubrics/2668/view" target="_blank"> Nanodegree Project Rubric Guide</a>
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -20,12 +20,12 @@ function handleSubmit(event) {
         fetch('http://localhost:8081/userText', responseOptions)
         .then(res => res.json())
         .then(function(res) {
-            document.querySelector('#model').innerHTML = 'Model: ' + res.model;
-            document.querySelector('#score_tag').innerHTML = 'Score: ' + res.score_tag;
-            document.querySelector('#agreement').innerHTML = 'Agreement: ' + res.agreement;
-            document.querySelector('#subjectivity').innerHTML = 'Subjectivity: ' + res.subjectivity;
-            document.querySelector('#confidence').innerHTML = 'Confidence: ' + res.confidence;
-            document.querySelector('#irony').innerHTML = 'Irony: ' + res.irony;
+            document.querySelector('#model').innerHTML = `<strong>Model:</strong>  ${res.model}`;
+            document.querySelector('#score_tag').innerHTML = `<strong>Score:</strong> ${res.score_tag}`;
+            document.querySelector('#agreement').innerHTML = `<strong>Agreement:</strong> ${res.agreement}`;
+            document.querySelector('#subjectivity').innerHTML = `<strong>Subjectivity:</strong> ${res.subjectivity}`;
+            document.querySelector('#confidence').innerHTML = `<strong>Confidence:</strong> ${res.confidence}`;
+            document.querySelector('#irony').innerHTML = `<strong>Irony:</strong> ${res.irony}`;
         })
         // .catch(error => console.log('error', error));
     } else {
